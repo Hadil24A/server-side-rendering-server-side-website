a71dd7e2d461789d7aeb327a32467f8d8df550d2
Hadil24A <hadil.ahmed@hva.nl>
2024-03-10 18:08:22 +0100

    aanpassingen
    svg'tjes
    server.js: route herschreven
    ejs: playlist data + styling

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 let navigatie = document.querySelector(".navigatie");
-let menu = document.querySelector(".home-menu")
+let menu = document.querySelector(".home-menu");
 
-navigatie.addEventListener('click', () => {
-    navigatie.classList.toggle("active");
-    menu.classList.toggle("home-menu-active")
+navigatie.addEventListener("click", () => {
+  navigatie.classList.toggle("active");
+  menu.classList.toggle("home-menu-active");
 });
--- server.js
+++ server.js
@@ -1,71 +1,61 @@
-// 1. Opzetten van de webserver
+                                                          // 1. Opzetten van de webserver
 
-// Importeer het npm pakket express uit de node_modules map
-import express from 'express'
+    // Importeer het npm pakket express uit de node_modules map
+import express from "express";
 
-// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
-import fetchJson from './helpers/fetch-json.js'
+    // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from "./helpers/fetch-json.js";
 
-// Haal alle stories uit de directus API op
-const stories = await fetchJson('https://fdnd-agency.directus.app/items/tm_story')
+    // Maak een nieuwe express app aan
+const app = express();
 
-// Maak een nieuwe express app aan
-const app = express()
+    // Stel ejs in als template engine
+app.set("view engine", "ejs");
 
-// Stel ejs in als template engine
-app.set('view engine', 'ejs')
+    // Stel de map met ejs templates in
+app.set("views", "./views");
 
-// Stel de map met ejs templates in
-app.set('views', './views')
+    // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static("public"));
 
-// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
-app.use(express.static('public'))
+    // Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({ extended: true }));
 
-// Zorg dat werken met request data makkelijker wordt
-app.use(express.urlencoded({extended: true}))
+                                                            // 2. Routes 
 
-// Zet een array klaar, 
-// const ...  = []
+   
+app.get("/", function (request, response) {      // Maak een GET route voor de homepage
+  response.render("homepage", {});      // Render homepage.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd stories
+});
 
-// 2. Routes die HTTP Request and Responses afhandelen
-
-// Maak een GET route voor de homepage
-app.get('/', function(request, response) {
-
-  // Haal alle stories uit de directus API op
-  fetchJson('https://fdnd-agency.directus.app/items/tm_story').then((stories) => {
-
-    // stories bevat gegevens van alle stories
-    // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
-
-    // Render homepage.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd stories
-    response.render('homepage', {
-    })
-  })
-})
-
-// Maak een GET route voor de lessons page
-app.get('/lessons', function(request, response) {
-
-// Haal alle stories uit de directus API op
-fetchJson('https://fdnd-agency.directus.app/items/tm_playlist').then((playlist) => {
-
-// stories bevat gegevens van alle stories
-
-// Render homepage.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd stories
-response.render('lessons', {
-  playlist: playlist})
-})
-})
+    
+app.get("/lessons", function (request, response) {      // Maak een GET route voor de lessons page
+  Promise.all([
+    fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
+    fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
+    fetchJson('https://fdnd-agency.directus.app/items/tm_playlist'),
+    fetchJson('https://fdnd-agency.directus.app/items/tm_audio')]).then(([storyData, languageData, playlistData, audioData]) => {    // Haal alle playlists uit de directus API op
+    response.render('lessons', {
+      stories: storyData.data, 
+      language: languageData.data,
+      playlist: playlistData.data,
+      audio: audioData.data})
+  });    // Render homepage.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd playlistData, storyData, languageData, audioData
+       
+});
 
 
 // 3. Start de webserver
 
 // Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+app.listen(app.get("port"), function () {
+// Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+
+
+
